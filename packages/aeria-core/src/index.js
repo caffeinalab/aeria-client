3507f0b2877661bbf01a5f26ea04f6e04da81331
Alberto Parziale <alberto.parziale@caffeina.com>
2020-01-14 10:35:30 +0100

    add some module exports

--- packages/aeria-core/src/index.js
+++ packages/aeria-core/src/index.js
@@ -1,1 +1,3 @@
-export default {}
+export { default as Context } from './src/components/Context'
+export { default as FieldsManager } from './src/components/FieldsManager'
+export { default as Module } from './src/components/Module'
--- packages/aeria-wp/src/utils/scroll-to.js
+++ packages/aeria-wp/src/utils/scroll-to.js
@@ -1,48 +1,51 @@
 
-  var requestAnimFrame = (function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||function(callback){window.setTimeout(callback,1000/60);};})();
+const requestAnimFrame = (function() {return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {window.setTimeout(callback, 1000 / 60)}})()
 
-  var easeInOutQuad = function (t, b, c, d) {
-      t /= d/2;
-      if (t < 1) return c/2*t*t + b;
-      t--;
-      return -c/2 * (t*(t-2) - 1) + b;
-  };
+const easeInOutQuad = function(t, b, c, d) {
+  t /= d / 2
+  if (t < 1) return c / 2 * t * t + b
+  t--
+  return -c / 2 * (t * (t - 2) - 1) + b
+}
 
-  var animatedScrollTo = function (element, offset, duration, callback) {
+const animatedScrollTo = function(element, offset, duration, callback) {
+  const to = window.pageYOffset + element.getBoundingClientRect().top - offset
+  const start = window.pageYOffset
 
-      var to = window.pageYOffset + element.getBoundingClientRect().top - offset
-      var start = window.pageYOffset,
-      change = to - start,
-      animationStart = Date.now();
-      var animating = true;
-      var lastpos = null;
 
-      var animateScroll = function() {
-          if (!animating) {
-              return;
-          }
-          requestAnimFrame(animateScroll);
-          var now = Date.now();
-          var val = Math.floor(easeInOutQuad(now - animationStart, start, change, duration));
-          if (lastpos) {
-              if (lastpos === window.pageYOffset) {
-                  lastpos = val;
-                  window.scrollTo(0,val);
-              } else {
-                  animating = false;
-                  if (callback) { callback(); }
-              }
-          } else {
-              lastpos = val;
-              window.scrollTo(0,val);
-          }
-          if (now > animationStart + duration) {
-              window.scrollTo(0,to);
-              animating = false;
-              if (callback) { callback(); }
-          }
-      };
-      requestAnimFrame(animateScroll);
-  };
+  const change = to - start
 
-  export default animatedScrollTo
+
+  const animationStart = Date.now()
+  let animating = true
+  let lastpos = null
+
+  var animateScroll = function() {
+    if (!animating) {
+      return
+    }
+    requestAnimFrame(animateScroll)
+    const now = Date.now()
+    const val = Math.floor(easeInOutQuad(now - animationStart, start, change, duration))
+    if (lastpos) {
+      if (lastpos === window.pageYOffset) {
+        lastpos = val
+        window.scrollTo(0, val)
+      } else {
+        animating = false
+        if (callback) { callback() }
+      }
+    } else {
+      lastpos = val
+      window.scrollTo(0, val)
+    }
+    if (now > animationStart + duration) {
+      window.scrollTo(0, to)
+      animating = false
+      if (callback) { callback() }
+    }
+  }
+  requestAnimFrame(animateScroll)
+}
+
+export default animatedScrollTo
